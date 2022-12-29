1102dbc00443c14b347d3b6cbffd138f1419a179
Devarsh-AD <devarsh@simpos.com.au>
2022-12-29 14:19:06 +0530

    seprate home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 import 'package:flut_keyboard/Bloc/bloc/key_board_bloc.dart';
 import 'package:flut_keyboard/constants/keyString.dart';
+import 'package:flut_keyboard/screens/Home_screen.dart';
 import 'package:flut_keyboard/widgets/flutter_keyboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -23,66 +24,6 @@ class MyApp extends StatelessWidget {
             ),
             body: Home()),
       ),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  TextEditingController keyPress = TextEditingController();
-  late KeyBoardBloc keyBoard;
-  @override
-  Widget build(BuildContext context) {
-    keyBoard = BlocProvider.of<KeyBoardBloc>(context);
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: BlocBuilder<KeyBoardBloc, KeyBoardState>(
-            builder: (context, state) {
-              if (state is GetKeyState) {
-                keyPress.text = state.keyString;
-              }
-              if (state is RemovedKeyState) {
-                if (keyPress.text.isNotEmpty) {
-                  keyPress.text =
-                      keyPress.text.substring(0, keyPress.text.length - 1);
-                }
-              }
-              return TextFormField(
-                controller: keyPress,
-                keyboardType: TextInputType.phone,
-                decoration: const InputDecoration(
-                    floatingLabelAlignment: FloatingLabelAlignment.center,
-                    labelText: 'Text',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(25.0)))),
-              );
-            },
-          ),
-        ),
-        FlutterKeyboard(
-            getPressedKey: (s) => {
-                  if (CommomManager.isCapsLockOff)
-                    {
-                      keyBoard.add(GetString(
-                          keyString: keyPress.text =
-                              keyPress.text + s.toLowerCase()))
-                    }
-                  else
-                    {
-                      keyBoard.add(GetString(
-                          keyString: keyPress.text = keyPress.text + s))
-                    }
-                })
-      ],
     );
   }
 }
---
+++ lib/screens/Home_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flut_keyboard/Bloc/bloc/key_board_bloc.dart';
+import 'package:flut_keyboard/constants/keyString.dart';
+import 'package:flut_keyboard/widgets/flutter_keyboard.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController keyPress = TextEditingController();
+  late KeyBoardBloc keyBoard;
+  @override
+  Widget build(BuildContext context) {
+    keyBoard = BlocProvider.of<KeyBoardBloc>(context);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: BlocBuilder<KeyBoardBloc, KeyBoardState>(
+            builder: (context, state) {
+              if (state is GetKeyState) {
+                keyPress.text = state.keyString;
+              }
+              if (state is RemovedKeyState) {
+                if (keyPress.text.isNotEmpty) {
+                  keyPress.text =
+                      keyPress.text.substring(0, keyPress.text.length - 1);
+                }
+              }
+              return TextFormField(
+                controller: keyPress,
+                keyboardType: TextInputType.phone,
+                decoration: const InputDecoration(
+                    floatingLabelAlignment: FloatingLabelAlignment.center,
+                    labelText: 'Text',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+              );
+            },
+          ),
+        ),
+        FlutterKeyboard(
+            getPressedKey: (s) => {
+                  if (CommomManager.isCapsLockOff)
+                    {
+                      keyBoard.add(GetString(
+                          keyString: keyPress.text =
+                              keyPress.text + s.toLowerCase()))
+                    }
+                  else
+                    {
+                      keyBoard.add(GetString(
+                          keyString: keyPress.text = keyPress.text + s))
+                    }
+                })
+      ],
+    );
+  }
+}
